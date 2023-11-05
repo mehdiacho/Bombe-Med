06a48b4910a8ca8bbc9fa5606531b238ba1c423b
EmekaOlaraonye <emekaajay@gmail.com>
2023-11-05 22:23:55 +0200

    removed sandbox

--- src/App.js
+++ src/App.js
@@ -3,18 +3,16 @@ import {Route, Routes } from "react-router-dom";
 import { AuthContextProvider} from "./config/auth-context";
 import Login from "./pages/Login";
 import Home from "./pages/Home"
-import Sandbox from "./pages/sandbox";
 import TrueSidebar from "./pages/components/TrueSidebar";
 
 function App() {
     return (
         <div className={"d-flex"}>
-            <TrueSidebar/>
+        
             <AuthContextProvider>
                 <Routes>
                     <Route path={'/'} element={<Login />} />
                     <Route path={'/home'} element={<Home />} />
-                    <Route path={'/sand'} element={<Sandbox />} />
                 </Routes>
             </AuthContextProvider>
 
